3e3c9f9195969eedf280c248f1cf873067147404
재현 한 <0501wogus@naver.com>
2022-04-10 23:06:43 +0900

    [ES] 06 하노이의탑 과제

---
+++ Lecture 05/Hanoi.js
@@ -0,0 +1,13 @@
+var RSquares = document.getElementById('RSquares');
+var YSquares = document.getElementById('YSquares');
+var GSquares = document.getElementById('GSquares');
+var BSquares = document.getElementById('BSquares');
+var PSquares = document.getElementById('PSquares');
+var BlSquares = document.getElementById('BlSquares');
+
+function Reset() 
+{
+    var hidden = document.getElementsByClassName("squares")[0];
+
+    hidden.style.visibility = "visible";
+}
